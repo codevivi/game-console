52f9ee8638fe6b1d7d75d5c1ea0f58e6fa652c4a
Vilma <vilmail@protonmail.com>
2023-02-16 10:00:35 +0200

    buttons onclick log correct action

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-'use strict'
+"use strict";
+const buttonsContainerEl = document.getElementById("buttons");
+buttonsContainerEl.addEventListener("click", onClick);
+
+function onClick(e) {
+  if (e.target.dataset.action) {
+    let action = e.target.dataset.action;
+    controller[action]();
+  }
+}
+const controller = {
+  screen: document.getElementById("screen"),
+
+  onOff: () => {
+    console.log("on off");
+  },
+  goTv: () => {
+    console.log("tv");
+  },
+  goBack: () => {
+    console.log("back");
+  },
+  goShop: () => {
+    console.log("shop");
+  },
+  goGoogle: () => {
+    console.log("google");
+  },
+};

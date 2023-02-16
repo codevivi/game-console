ee4be4900b3dbde6672995fab126eff62b465ff8
Vilma <vilmail@protonmail.com>
2023-02-16 22:05:24 +0200

    bye message, iframe swiches, left todo game shop

--- README.md
+++ README.md
@@ -14,6 +14,6 @@ HTML, CSS, JS
 - [x] **Top white** joystick button turns on TV. TV shows hello message. Same button switches off TV with goodbye message.
 - [x] **Y yellow** button turns on tv program.Put this code:
       `<iframe width="100%" height="315" src="https://www.youtube.com/embed/y-28CssnqEE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>` inside tv frame.
-- [ ] **B red** button takes user back to hello screen;
+- [x] **B red** button takes user back to hello screen;
 - [ ] **X blue** button shows possible to buy games list. Every game is listed with picture, name and price. Games must be stored in array.
-- [ ] **A green** button switches on google.com page. (hint?: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe)
+- [x] **A green** button switches on google.com page. (hint?: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe)
--- main.js
+++ main.js
@@ -1,36 +1,86 @@
 "use strict";
 const buttonsContainerEl = document.getElementById("buttons");
-buttonsContainerEl.addEventListener("click", onClick);
+buttonsContainerEl.addEventListener("click", changeView);
 
-function onClick(e) {
+function changeView(e) {
   if (e.target.dataset.action) {
     let action = e.target.dataset.action;
-    controller[action]();
+    viewController[action]();
   }
 }
-const controller = {
-  view: document.getElementById("view"),
-  iframe: this.view.querySelector(".iframe"),
-
+const viewController = {
+  isOn: false,
+  helloMsg: "Hello! homeworks.done ? X : A",
+  byeMsg: "Good bye...",
+  viewEl: document.getElementById("view"),
+  msgEl: document.querySelector(".home-msg"),
+  iframeEl: null,
   onOff: function () {
-    console.log("on off");
-    this.view.classList.toggle("off");
-    if (!this.iframe.classList.contains("off")) {
-      this.iframe.classList.add("off");
+    this.removeIframe();
+    if (this.isOn) {
+      this.msgEl.textContent = this.byeMsg;
+      buttonsContainerEl.removeEventListener("click", changeView);
+      setTimeout(() => {
+        this.viewEl.classList.toggle("off");
+        this.isOn = !this.isOn;
+        buttonsContainerEl.addEventListener("click", changeView);
+      }, 1500);
+    } else {
+      this.msgEl.textContent = this.helloMsg;
+      this.viewEl.classList.toggle("off");
+      this.isOn = !this.isOn;
     }
   },
   goTv: function () {
-    if (this.iframe.classList.contains("off")) {
-      this.iframe.classList.remove("off");
-    }
+    const el = createYoutubeIframe("https://www.youtube.com/embed/y-28CssnqEE");
+    this.addIframe(el);
   },
   goBack: function () {
     console.log("back");
+    this.removeIframe();
   },
   goShop: function () {
     console.log("shop");
   },
   goGoogle: function () {
-    console.log("google");
+    this.addIframe(createIframe("https://www.google.com/search?igu=1"));
+  },
+  addIframe(el) {
+    this.viewEl.classList.add("loading");
+    if (this.iframeEl) {
+      this.iframeEl.replaceWith(el);
+      this.iframeEl = el;
+    } else {
+      this.viewEl.prepend(el);
+      this.iframeEl = el;
+    }
+  },
+  removeIframe() {
+    this.viewEl.classList.remove("loading");
+    if (this.iframeEl) {
+      this.iframeEl.remove();
+      this.iframeEl = null;
+    }
   },
 };
+function createIframe(src) {
+  const el = document.createElement("iframe");
+  el.src = src;
+  return el;
+}
+function createYoutubeIframe(src) {
+  const el = createIframe(src);
+  el.width = "100%";
+  el.height = "315";
+  el.title = "Youtube video player";
+  el.frameborder = "0";
+  el.allow = "accelerometer";
+  el.setAttribute("autoplay", true);
+  el.setAttribute("clipboard-write", true);
+  el.setAttribute("encrypted-media", true);
+  el.setAttribute("gyroscope", true);
+  el.setAttribute("picture-in-picture", true);
+  el.setAttribute("web-share", true);
+  el.setAttribute("allowfullscreen", true);
+  return el;
+}
